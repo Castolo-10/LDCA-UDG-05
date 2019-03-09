bb2bec1ac65379ac9fea101592ae59137cd85f7e
Castolo-10 <leonardo.castolo@alumnos.udg.mx>
2019-03-09 01:58:54 -0600

    Ajustes al socket

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Crear conexión
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect('https://ldca-udg-05.herokuapp.com');
 
 // Query DOM
 var message = document.getElementById('message'),
